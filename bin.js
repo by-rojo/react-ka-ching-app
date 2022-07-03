ca53aa22530e9e1daa5455b3970d806ee149c23f
Roe Greene <robert@byrojo.com>
2022-07-03 15:31:22 -0700

    add install script

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const [, ,] = process.argv;
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 const { exec } = require("child_process");
 
 const readline = require("readline").createInterface({
@@ -29,6 +29,8 @@ const questions = {
 
 const questionObjectKeys = Object.keys(questions);
 
+const projectPath = () => path.join(process.cwd(), questions.name.answer);
+
 const askQuestion = () => {
   return new Promise((resolve) => {
     const key = questionObjectKeys.shift();
@@ -45,26 +47,39 @@ const askQuestion = () => {
   });
 };
 
-const projectPath = () => path.join(process.cwd(), questions.name.answer)
-
 const generateProject = () => {
   return new Promise((resolve, reject) => {
-    fs.mkdir(projectPath(), (err => {
-        if(err) reject(err)
-        else resolve()
-    }))
+    fs.mkdir(projectPath(), (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
   });
 };
 
 const downloadWebFE = () => {
-    return new Promise((resolve, reject) => {
-        exec(`curl -sL https://github.com/by-rojo/iagnmft-nodejs/zipball/main/ | tar zx --strip-components 1 -C ${projectPath()}`, (error, output) => {
-            console.log(output)
-            if(error) reject(error)
-            else resolve()
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    console.log("Downloading the frontend");
+    exec(
+      `curl -sL https://github.com/by-rojo/iagnmft-nodejs/zipball/main/ | tar zx --strip-components 1 -C ${projectPath()}`,
+      (error, output) => {
+        console.log(error || output);
+        if (error) reject(error);
+        else resolve();
+      }
+    );
+  });
+};
+
+const installWebFE = () => {
+  console.log("Installing the frontend");
+  return new Promise((resolve, reject) => {
+    exec(`cd ${projectPath()} && npm i`, (err, output) => {
+      console.log(err || output || "Installed the frontend");
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+};
 
 const exit = () => {
   readline.close();
@@ -73,4 +88,5 @@ const exit = () => {
 askQuestion()
   .then(() => generateProject())
   .then(() => downloadWebFE())
+  .then(() => installWebFE())
   .then(exit);
