f041717e24bf5b115e462c0d4d44406948faa271
Roe Greene <robert@byrojo.com>
2022-07-03 14:51:28 -0700

    add download script

--- bin.js
+++ bin.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 const [, ,] = process.argv;
+const fs = require("fs");
+const path = require("path")
+const { exec } = require("child_process");
 
 const readline = require("readline").createInterface({
   input: process.stdin,
@@ -27,16 +30,47 @@ const questions = {
 const questionObjectKeys = Object.keys(questions);
 
 const askQuestion = () => {
-  const key = questionObjectKeys.shift();
-  if (key) {
-    const questionObject = questions[key]
-    readline.question(`${questionObject.question}: `, (answer) => {
-        questions[key].answer = key === "name" ? answer.toLocaleLowerCase().replace(/ /g, "-") : answer;
-        askQuestion()
-    });
-  } else {
-    readline.close();
-  }
+  return new Promise((resolve) => {
+    const key = questionObjectKeys.shift();
+    if (key) {
+      const questionObject = questions[key];
+      readline.question(`${questionObject.question}: `, (answer) => {
+        questions[key].answer =
+          key === "name"
+            ? answer.toLocaleLowerCase().replace(/ /g, "-")
+            : answer;
+        resolve(askQuestion());
+      });
+    } else resolve();
+  });
+};
+
+const projectPath = () => path.join(process.cwd(), questions.name.answer)
+
+const generateProject = () => {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(projectPath(), (err => {
+        if(err) reject(err)
+        else resolve()
+    }))
+  });
+};
+
+const downloadWebFE = () => {
+    return new Promise((resolve, reject) => {
+        exec(`curl -sL https://github.com/by-rojo/iagnmft-nodejs/zipball/main/ | tar zx --strip-components 1 -C ${projectPath()}`, (error, output) => {
+            console.log(output)
+            if(error) reject(error)
+            else resolve()
+        })
+    })
+}
+
+const exit = () => {
+  readline.close();
 };
 
 askQuestion()
+  .then(() => generateProject())
+  .then(() => downloadWebFE())
+  .then(exit);
